3e90a7af272097eea5e437784db9a1643c8f48c0
june <juneyjw92@gmail.com>
2023-02-05 15:41:20 +0900

    initial commit

---
+++ Desktop/GitTest/GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by June on 2023/02/05.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
